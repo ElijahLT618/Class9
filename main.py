d87d7d0bdebdc45805a14182f72301e4cfe79ef1
Elijah Theisen <elijahltheisen@gmail.com>
2023-12-15 12:30:11 -0600

    changes to main.py

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config[' DEBUG '] = True
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://class9:beproductive@localhost:8889/class9'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+
+    def __init__(self, name):
+        self.name = name
+
+
 
 tasks = []
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
         task = request.form['task']
         tasks.append(task)
-        
-    
+
     return render_template('todos.html',title="Get It Done!", tasks=tasks)
 
-app.run()
+
+if __name__ == '__main__':
+    app.run()
