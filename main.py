bbb4122581618fd5e1c053de404a15cb9746ead9
Elijah Theisen <elijahltheisen@gmail.com>
2023-12-19 22:24:53 -0600

    hotfix

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done-c9:890test890@localhost:8889/get-it-done-c9'
 app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 class Task(db.Model):
 
